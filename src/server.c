d6b8344b3965e6f383ea7dccd518ad6f9e8e99d3
vcvc-g <qgao10@illinois.edu>
2019-09-20 16:45:57 -0500

    update port

--- src/server.c
+++ src/server.c
@@ -15,8 +15,8 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-// #define PORT "3490"  // the port users will be connecting to
-#define PORT "80"  // the http port users will be connecting to
+#define PORT "3490"  // the port users will be connecting to
+// #define PORT "80"  // the http port users will be connecting to
 #define MAXDATASIZE 100 
 #define BACKLOG 10	 // how many pending connections queue will hold
 
