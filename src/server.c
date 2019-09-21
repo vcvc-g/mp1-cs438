362d67d5d1fefa2b8112648e9bb49078b5762e5a
vcvc-g <qgao10@illinois.edu>
2019-09-21 16:28:43 -0500

    server before content length

--- src/server.c
+++ src/server.c
@@ -1,7 +1,3 @@
-/*
-** server.c -- a stream socket server demo
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,7 +14,7 @@
 #define PORT "3490"  // the port users will be connecting to
 #define MAXDATASIZE 100 
 #define BACKLOG 10	 // how many pending connections queue will hold
-#define HTTP200 "HTTP/1.1 200 OK \r\n\r\n"
+#define HTTP200 "HTTP/1.1 200 OK\r\n\r\n"
 #define HTTP400 "HTTP/1.1 400 Bad Request\r\n\r\n"
 #define HTTP404 "HTTP/1.1 404 Not Found\r\n\r\n"
 
@@ -50,8 +46,6 @@ int main(int argc, char *argv[])
 	int rv;
 	
 	char wget_buf[MAXDATASIZE];
-	char file_buf[MAXDATASIZE];
-	int numfread; 
 	FILE *fd;
 
 	if (argc != 2) {
@@ -131,8 +125,6 @@ int main(int argc, char *argv[])
 
 		if (!fork()) { // this is the child process
 			close(sockfd); // child doesn't need the listener
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send failed");
 			
 			if (recv(new_fd, wget_buf, MAXDATASIZE-1, 0) == -1){ // if recv error
 				if (send(new_fd, HTTP400, strlen(HTTP400), 0) == -1)
@@ -159,8 +151,6 @@ int main(int argc, char *argv[])
 				strcat(filePath,getParse[1]);
 				printf("\nFile Name Received: %s\n",filePath); 
 				fd = fopen(filePath, "r"); 
-				// fd = fopen(wget_buf, "r"); 
-				// printf("\nFile Name Received: %s\n", wget_buf); 	
 
 				if (fd == NULL) { //invalid file
 					printf("\nFile open failed!\n"); 
@@ -168,45 +158,35 @@ int main(int argc, char *argv[])
 						perror("send failed");
 				}
 				else{
-					char temp_buf[MAXDATASIZE];
+					char file_buf[MAXDATASIZE];
+					int numfread, numsend, fsize;
+					fseek(fd, 0, SEEK_END); // seek to end of file
+					fsize = ftell(fd); // get current file pointer
+					fseek(fd, 0, SEEK_SET); // seek back to beginning of file
 					printf("\nFile Successfully opened!\n");
-					memcpy(temp_buf,HTTP200,strlen(HTTP200));
-					numfread = fread(file_buf,1,MAXDATASIZE-strlen(HTTP200),fd);
-					memcpy(&temp_buf[strlen(HTTP200)],file_buf,numfread);
-					send(new_fd, file_buf, numfread+strlen(HTTP200), 0);
-
-					do {
-						memset(file_buf,0,sizeof(file_buf));
-						numfread = fread(file_buf, 1, MAXDATASIZE, fd);
-						if (numfread<MAXDATASIZE){
-							break;
-						}
-						file_buf[numfread] = '\0';
-						if (send(new_fd, file_buf, numfread, 0) == -1) {
+					memset(file_buf,'\0',MAXDATASIZE);
+					strcpy(file_buf,HTTP200);
+					numsend=send(new_fd, file_buf, strlen(file_buf), 0);
+					printf("\nHTTP Response send:%d, filebuf:%lu\n",numsend,strlen(file_buf));
+					while ((numfread = fread(file_buf, sizeof(char), MAXDATASIZE, fd))!=0) {
+						if ((numsend=send(new_fd, file_buf, numfread, 0)) == -1) {
 							perror("send failed");
 							fclose(fd);
 						}
-					} while (numfread);
-					// char buf_out[MAXDATASIZE];
-					// while((numfread = fread(buf_out, sizeof(char), MAXDATASIZE, fd)) != 0) {
-					// 	if(send(new_fd, buf_out, numfread, 0) == -1){
-					// 		perror("send");
-					// 		fclose(fd);
-					// 	}
-					// }
-
+						printf("\nFile sned:%d, filebuf:%lu, numfread:%d, fsieze:%d\n",numsend,strlen(file_buf),numfread,fsize);
+						memset(file_buf,'\0',sizeof(file_buf));
+							
+					};
 					fclose(fd);
-					printf("\nFile closed\n");
+					printf("File closed");
 				}
 			}
-			printf("\nnew_fd closed\n");
+
 			close(new_fd);
 			exit(0);
 		}
-		printf("\nparent end\n");
 		close(new_fd);  // parent doesn't need this
 	}
 
 	return 0;
 }
-
